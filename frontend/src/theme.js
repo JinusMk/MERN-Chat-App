0e434de6e29b22af0c42db9efdf226062644c02c
Jinus-MK <jinus.mk@cuemath.com>
2024-10-31 23:26:37 +0530

    fix: fontsource

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -1,9 +1,5 @@
 import { extendTheme } from '@chakra-ui/react';
 import '@fontsource/inter';
-import "@fontsource/inter/400.css";
-import "@fontsource/inter/500.css";
-import "@fontsource/inter/600.css";
-import "@fontsource/inter/700.css";
 
 const COLORS = {
   transparent: 'transparent',
